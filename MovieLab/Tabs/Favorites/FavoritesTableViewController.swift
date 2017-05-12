5a706fb0d7c3c5d8315df678fbc5416b291e3e18
aarthur <tricpony@mac.com>
2017-05-12 01:34:30 -0400

    added code to initialize the MOC in viewDidLoad

--- MovieLab/Tabs/Favorites/FavoritesTableViewController.swift
+++ MovieLab/Tabs/Favorites/FavoritesTableViewController.swift
@@ -16,11 +16,9 @@ class FavoritesTableViewController: UITableViewController, NSFetchedResultsContr
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        self.managedObjectContext = CoreDataStack.sharedInstance().mainContext
+        self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     // MARK: - Fetched results controller
